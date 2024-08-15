4a7272e8b44a8b41d42d8004e49bd5f26b96b35b
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-08-15 14:38:18 -0500

    mi primer programita en java

---
+++ main.java
@@ -0,0 +1,18 @@
+class Main {
+    public static void main(String args[]){
+        int x = 0;
+        float y = 0.5f; //f para saber que es variable de tipo float
+        double z = 10000;
+        boolean b = true;
+        boolean i = false; 
+         System.out.print("hola zelaya te qleo");
+         
+
+    }
+
+
+
+
+
+
+}
